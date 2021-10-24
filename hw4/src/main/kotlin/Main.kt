9f9c8dd4bbcef83029b4bf2fbe37746ab88f9dad
Alexey Kislovskiy <kislovskij.a@mail.ru>
2021-10-24 20:51:05 +0300

    update logic

--- hw4/src/main/kotlin/Main.kt
+++ hw4/src/main/kotlin/Main.kt
@@ -1,16 +1,16 @@
-import arrow.core.andThen
 import arrow.core.partially1
 
-fun calculate(vararg params: Pair<(Int) -> Int, Int>): Int {
-    val fnc = params.map {
-        val v = it.first.partially1(it.second)
-        val f = { x: Int -> x + v() }
-        f
-    }.reduce { c, n -> c andThen n }
-    return fnc(0)
+fun main(args: Array<String>) {
+    parse(args)
+    val val1 = args[0].toInt()
+    val operator = args[1]
+    val val2 = args[2].toInt()
+    val ans = when (operator) {
+        "+" -> calculate(Pair(add.partially1(val1), val2));
+        "-" -> calculate(Pair(subtract.partially1(val1), val2));
+        "/" -> calculate(Pair(divide.partially1(val1), val2));
+        "*" -> calculate(Pair(multiply.partially1(val1), val2));
+        else -> 0;
+    }
+    println(ans)
 }
-
-val add = { x: Int, y: Int -> x + y }
-val subtract = { x: Int, y: Int -> x - y }
-val divide = { x: Int, y: Int -> x / y }
-val multiply = { x: Int, y: Int -> x * y }
